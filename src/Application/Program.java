97b1ef482dc7a03f88aa7111dfe7547c86fd1319
A1c296?? <anacaroline010209@gmail.com>
2025-12-23 15:11:43 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,6 +1,9 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,10 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(2, "Bob", "bob@email.com", new Date(), 3000.0, new Department(1, "Books"));
+		
+		System.out.println(seller);
 
 	}
 
