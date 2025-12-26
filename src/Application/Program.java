1f75aae4c26715eae6b7985d8c792792e204ea2d
A1c296?? <anacaroline010209@gmail.com>
2025-12-26 17:48:03 -0300

    findById implementation

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -2,6 +2,8 @@ package Application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,10 +11,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(2, "Bob", "bob@email.com", new Date(), 3000.0, new Department(1, "Books"));
+	  SellerDao sellerDao = DaoFactory.createSellerDao();
+	  
+	  Seller seller = sellerDao.findById(3);
+	  
 		
 		System.out.println(seller);
 
